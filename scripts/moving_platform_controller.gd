29c20c8775105e775a03112aba3749a5c3cc117e
Nigel Karunaratne <nigel.karunaratne36@gmail.com>
2023-09-22 21:49:25 -0400

    Moving Platforms now use Tweens
    a tween is created on each platform's _ready function.

--- scripts/moving_platform_controller.gd
+++ scripts/moving_platform_controller.gd
@@ -1,31 +1,41 @@
-extends Node3D
+extends Path3D
 
 @onready var path_follow : PathFollow3D = $PathFollow3D
 @onready var platform : AnimatableBody3D = $PathFollow3D/AnimatableBody3D
 
 enum MovingPlatformType {
 	LINEAR_BACK_AND_FORTH,
-	EASED_BACK_AND_FORTH
+	QUAD_BACK_AND_FORTH
 }
 
 @export var speed = 10
+@export var end_delay = 1.5
 @export var type: MovingPlatformType = MovingPlatformType.LINEAR_BACK_AND_FORTH
 @export var interp_curve: Curve
 
-var _going_forward = true
-
+var _tween : Tween
 
 func _ready():
 	# match type:
 	# 	MovingPlatformType.BACK_AND_FORTH:
 	path_follow.loop = false
+	_tween = create_tween()
 
-func _process(delta):
 	match type:
 		MovingPlatformType.LINEAR_BACK_AND_FORTH:
-			if (_going_forward and path_follow.progress_ratio >= 1) or (not _going_forward and path_follow.progress_ratio <= 0):
-				_going_forward = not _going_forward
-			path_follow.progress += delta * speed * (1 if _going_forward else -1)
+			_tween.set_trans(Tween.TRANS_LINEAR)
+			_tween.set_loops()
+			_tween.tween_property(path_follow, "progress", curve.get_baked_length(), curve.get_baked_length() / speed)
+			_tween.tween_interval(end_delay)
+			_tween.tween_property(path_follow, "progress", 0, curve.get_baked_length() / speed)
+			_tween.tween_interval(end_delay)
+		MovingPlatformType.QUAD_BACK_AND_FORTH:
+			_tween.set_trans(Tween.TRANS_QUAD)
+			_tween.set_loops()
+			_tween.tween_property(path_follow, "progress", curve.get_baked_length(), curve.get_baked_length() / speed)
+			_tween.tween_interval(end_delay)
+			_tween.tween_property(path_follow, "progress", 0, curve.get_baked_length() / speed)
+			_tween.tween_interval(end_delay)
 
-
-	return
+func end_tween():
+	_tween.kill();
