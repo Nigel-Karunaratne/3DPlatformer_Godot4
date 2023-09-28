19936253c1636352c975f036cf9ab9099dcd7ddd
Nigel Karunaratne <nigel.karunaratne36@gmail.com>
2023-09-28 19:18:27 -0400

    Now works with grounded influence velocity

--- scripts/accelerator_pad.gd
+++ scripts/accelerator_pad.gd
@@ -13,8 +13,10 @@ func _physics_process(_delta):
 func _on_area_3d_body_entered(body:Node3D):
 	if body is PlayerMove:
 		player = (body as PlayerMove)
+		player.on_accel_pad = true
 
 func _on_area_3d_body_exited(body:Node3D):
 	if body == player:
 		# player.influence_velocity -= -basis.z * applied_force
+		player.on_accel_pad = false
 		player = null
