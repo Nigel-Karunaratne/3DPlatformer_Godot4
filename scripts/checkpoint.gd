7f71711c0d76fccf82ca04515fd1f2e94c0ec660
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2024-01-17 11:12:30 -0500

    Checkpoints won't reactivate when already active
    if a player activates a checkpoint, they cannot reactivate it until a new checkpoint is activated

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -7,5 +7,7 @@ func _on_body_entered(body):
 	if body is PlayerMove:
 		# TODO - Find a better way PLEASE
 		var gm = get_tree().get_root().get_child(0).find_child("GameManagerNode")
-		(gm as GameManager).on_checkpoint_enter(self)
-		pass
+		if (gm as GameManager).current_checkpoint != self:
+			(gm as GameManager).on_checkpoint_enter(self)
+		else:
+			print("DO NOTHING!")
