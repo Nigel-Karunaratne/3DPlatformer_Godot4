449ebfccd2eba977ce485fe6248ac0f39bdb2d69
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2024-01-17 11:25:00 -0500

    Cleaned up comments + useless code

--- editor_scripts/_find_all_checkpoints.gd
+++ editor_scripts/_find_all_checkpoints.gd
@@ -5,22 +5,22 @@ extends EditorScript
 
 func _run():
 	print("Starting... Looking for checkpoints")
-	var gm : GameManager = get_scene().find_child("GameManagerNode") as GameManager
 	
+	# Get GameManager. Abort if not present
+	var gm : GameManager = get_scene().find_child("GameManagerNode") as GameManager
 	if gm == null:
 		printerr("Error: Level has no GameManagerNode! Script Ended.")
 		return
-	
-	#print(get_scene().get_tree().get_nodes_in_group("Checkpoint").size())
-	print(get_scene().get_tree().get_nodes_in_group("Checkpoint"))
+
+	# Find all checkpoints in level
 	var checkpoints : Array = get_scene().get_tree().get_nodes_in_group("Checkpoint")
-	# gm.checkpoints = get_scene().get_tree().get_nodes_in_group("Checkpoint")
-	
-#	gm.checkpoints = Array(checkpoints)
+
+	# Add to GameManger's Array
 	gm.checkpoints = Array()
 	for chp in checkpoints:
 		gm.checkpoints.append((chp as Checkpoint))
 		if (chp as Checkpoint).is_level_start:
+			# Set GameManger's current_checkpoint to chp
 			gm.current_checkpoint = (chp as Checkpoint)
 			(chp as Node3D).position = (gm as GameManager).player_ref.position
 			print("Moving a checkpoint with 'is_level_start' to player_ref's current location")
--- editor_scripts/_find_all_collectables.gd
+++ editor_scripts/_find_all_collectables.gd
@@ -5,22 +5,22 @@ extends EditorScript
 
 func _run():
 	print("Starting...")
+
+	# Get GameManager. Abort if not present
 	var gm : GameManager = get_scene().find_child("GameManagerNode") as GameManager
-	
-#	var collectables = get_scene().get_tree().get_nodes_in_group("Collectable")
+	if gm == null:
+		printerr("Error: Level has no GameManagerNode! Script Ended.")
+		return
+
+	# Get Total # of Collectable nodes in scene
 	var collectable_count : int = get_scene().get_tree().get_nodes_in_group("Collectable").size()
 	gm.level_collectable_count = collectable_count
-	
-	
+
+
 #	var game_ui : Control = get_scene().find_child("GameUIControl")
 #	(game_ui.find_child("DCollectableTotalLabel", true) as Label).text = str(collectable_count)
-	
+
 #	for node in collectables:
 #		(node as Collectable).gm = gm
-	
-	#for node in get_all_children(get_scene()):
-		#if node is OmniLight3D:
-		#	pass
-		
-		
+
 	print("Ended. Found ", collectable_count, " collectables. The Game Manager has been updated.")
