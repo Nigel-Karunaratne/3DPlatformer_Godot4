7fc60112089e25c1aa60fe09f595afc81fcfb55c
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2024-01-22 17:39:22 -0500

    Updated Default Values

--- scripts/user_settings.gd
+++ scripts/user_settings.gd
@@ -4,8 +4,8 @@ extends Node
 
 var m_cam_x_sense : float = 0.25
 var m_cam_y_sense : float = 0.25
-var m_invert_x : bool = false
-var m_invert_y : bool = true
+var m_invert_x : bool = true
+var m_invert_y : bool = false
 
 var c_cam_x_sense : int = 150
 var c_cam_y_sense : int = 150
@@ -18,8 +18,8 @@ var s_sfx_level : float = 1.0
 const DEFAULTS = {
 	"m_cam_x_sense": 0.25,
 	"m_cam_y_sense": 0.25,
-	"m_invert_x": false,
-	"m_invert_y": true,
+	"m_invert_x": true,
+	"m_invert_y": false,
 	
 	"c_cam_x_sense": 150,
 	"c_cam_y_sense": 150,
