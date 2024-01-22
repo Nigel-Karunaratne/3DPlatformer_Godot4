205a848629fa984786832b1a2874c9d7417d30eb
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2024-01-22 11:56:03 -0500

    Now properly get scene node
    still need to find a better way of getting gamemanager :(

--- scripts/collectable.gd
+++ scripts/collectable.gd
@@ -7,7 +7,8 @@ func _on_area_3d_body_entered(body):
 	if body is PlayerMove:
 		
 		# TODO - find a better way PLEASE
-		gm = get_tree().get_root().get_child(0).find_child("GameManagerNode")
+		var root = get_tree().get_root()
+		gm = root.get_child(root.get_child_count()-1).find_child("GameManagerNode")
 		gm.increment_collectable_count(self)
 		
 		# TODO - Play Sound
--- scripts/kill_volume.gd
+++ scripts/kill_volume.gd
@@ -2,5 +2,6 @@ extends Area3D
 
 func _on_body_entered(body):
 	if body is PlayerMove:
-		var gm = get_tree().get_root().get_child(0).find_child("GameManagerNode") as GameManager
+		var root = get_tree().get_root()
+		var gm = root.get_child(root.get_child_count()-1).find_child("GameManagerNode") as GameManager
 		gm.player_die()
