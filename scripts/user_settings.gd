5e095e52df6c8d232a5a18325dba5fc1786a03ed
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2024-01-23 12:33:41 -0500

    Added saving and loading functions
    uses ConfigFile class
    can create default file if file is missing (for first time running)

--- scripts/user_settings.gd
+++ scripts/user_settings.gd
@@ -1,6 +1,7 @@
 extends Node
 
 # AUTOLOAD : UserSettings
+const FILE_PATH = "user://usersettings.cfg"
 
 var m_cam_x_sense : float = 0.25
 var m_cam_y_sense : float = 0.25
@@ -29,3 +30,69 @@ const DEFAULTS = {
 	"s_music_level": 1.0,
 	"s_sfx_level": 1.0
 }
+
+func save_settings():
+	var config = ConfigFile.new()
+	config.set_value("M", "m_cam_x_sense", m_cam_x_sense)
+	config.set_value("M", "m_cam_y_sense", m_cam_y_sense)
+	config.set_value("M", "m_invert_x", m_invert_x)
+	config.set_value("M", "m_invert_y", m_invert_y)
+	
+	config.set_value("C", "c_cam_x_sense", c_cam_x_sense)
+	config.set_value("C", "c_cam_y_sense", c_cam_y_sense)
+	config.set_value("C", "c_invert_x", c_invert_x)
+	config.set_value("C", "c_invert_y", c_invert_y)
+	
+	config.set_value("S", "s_music_level", s_music_level)
+	config.set_value("S", "s_sfx_level", s_sfx_level)
+	var error = config.save(FILE_PATH)
+	if error != OK:
+		printerr("Error w/ save_settings: ", error)
+	return
+
+func load_settings():
+	var config = ConfigFile.new()
+	var error = config.load(FILE_PATH)
+	
+	if error != OK:
+		print("Error while loading file: ", error)
+		if error == ERR_FILE_NOT_FOUND: # TODO - Handle more errors? Log errors?
+			_save_default_settings_file()
+		return
+	
+	config.get_value("M", "m_cam_x_sense", DEFAULTS["m_cam_x_sense"])
+	config.get_value("M", "m_cam_y_sense", DEFAULTS["m_cam_y_sense"])
+	config.get_value("M", "m_invert_x", DEFAULTS["m_invert_x"])
+	config.get_value("M", "m_invert_y", DEFAULTS["m_invert_y"])
+	
+	config.get_value("C", "c_cam_x_sense", DEFAULTS["c_cam_x_sense"])
+	config.get_value("C", "c_cam_y_sense", DEFAULTS["c_cam_y_sense"])
+	config.get_value("C", "c_invert_x", DEFAULTS["c_invert_x"])
+	config.get_value("C", "c_invert_y", DEFAULTS["c_invert_y"])
+	
+	config.get_value("S", "s_music_level", DEFAULTS["s_music_level"])
+	config.get_value("S", "s_sfx_level", DEFAULTS["s_sfx_level"])
+	return
+	
+func _save_default_settings_file():
+	var config = ConfigFile.new()
+	config.set_value("M", "m_cam_x_sense", DEFAULTS["m_cam_x_sense"])
+	config.set_value("M", "m_cam_y_sense", DEFAULTS["m_cam_y_sense"])
+	config.set_value("M", "m_invert_x", DEFAULTS["m_invert_x"])
+	config.set_value("M", "m_invert_y", DEFAULTS["m_invert_y"])
+	
+	config.set_value("C", "c_cam_x_sense", DEFAULTS["c_cam_x_sense"])
+	config.set_value("C", "c_cam_y_sense", DEFAULTS["c_cam_y_sense"])
+	config.set_value("C", "c_invert_x", DEFAULTS["c_invert_x"])
+	config.set_value("C", "c_invert_y", DEFAULTS["c_invert_y"])
+	
+	config.set_value("S", "s_music_level", DEFAULTS["s_music_level"])
+	config.set_value("S", "s_sfx_level", DEFAULTS["s_sfx_level"])
+	var error = config.save(FILE_PATH)
+	if error != OK:
+		printerr("Error with creating a default config file: ", error)
+	return
+
+func _ready():
+	load_settings()
+	return
